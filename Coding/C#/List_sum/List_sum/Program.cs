01410f931fc52fac48a52e0667b5b23473561462
Sanin2009 <serov.sandro@gmail.com>
2022-09-22 18:44:36 +0300

    22.09.2022

---
+++ Coding/C#/List_sum/List_sum/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Дан List<uint> list и некое число ulong sum. Ожидаемое количество элементов
+// в list - несколько миллионов. Необходимо написать метод FindElementsForSum,
+// который сможет найти наименьшие индексы start и end такие, что сумма
+// элементов list начиная с индекса start включительно и до индекса end не
+// включительно будет в точности равна sum. Если таких start и end нельзя найти,
+// то установить start и end равными 0. Решение предоставить в виде метода.
+// Сигнаруту и название метода менять нельзя, только тело.
+
+
+
+
+namespace List_sum
+{
+    internal class Program
+    {
+        static class solver
+        {
+            static public void FindElementsForSum(List<uint> list, ulong sum, out int start, out int end)
+            {
+                start = 0;
+                end = 0;
+                ulong s = 0;
+                while (true)
+                {
+                    if (s == sum) return;
+                    else if(s<sum)
+                    {
+                        if (end!=list.Count) s += list[end];
+                        end++;
+                    }
+                    else if(s>sum)
+                    {
+                        s-=list[start];
+                        start++;
+                    }
+                    if (end == list.Count+1)
+                    {
+                        start = 0;
+                        end = 0;
+                        return;
+                    }
+                }
+
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int start;
+            int end;
+            solver.FindElementsForSum(new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 }, 11, out start, out end); //start будет равен 5 и end 7;
+            Console.WriteLine(start + " " + end);
+            solver.FindElementsForSum(new List<uint> { 4, 5, 6, 7 }, 18, out start, out end); //start будет равен 1 и end 4;
+            Console.WriteLine(start + " " + end);
+            solver.FindElementsForSum(new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 }, 88, out start, out end); //start будет равен 0 и end 0;
+            Console.WriteLine(start + " " + end);
+            solver.FindElementsForSum(new List<uint> { 100, 1, 22, 33, 31, 2, 6, 7 }, 88, out start, out end); //start будет равен 2 и end 6;
+            Console.WriteLine(start + " " + end);
+            solver.FindElementsForSum(new List<uint> { 100, 1, 22, 33, 31, 2, 6, 97 }, 103, out start, out end); //start будет равен 6 и end 8;
+            Console.WriteLine(start + " " + end);
+        }
+    }
+}
---
+++ Coding/C#/input_output/Options.cs
@@ -0,0 +1,115 @@
+﻿// класс реализации загрузки и сохранения в игре 100_ROUNDS
+
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Form
+{
+    public class Options
+    {
+        public static class Option
+        {
+            public static int font;
+            public static bool time;
+            public static int rounds;
+            public static double res;
+            public static void load()
+            {
+                if (File.Exists("options.dll"))
+                {
+                    var data = File
+                                       .ReadAllLines("options.dll")
+                                       .Select(x => x.Split('='))
+                                       .Where(x => x.Length > 1)
+                                       .ToDictionary(x => x[0].Trim(), x => x[1]);
+                    font = Int32.Parse(data["font"]);
+                    time = bool.Parse(data["time"]);
+                    rounds = Int32.Parse(data["rounds"]);
+                    rounds -= 1024;
+                    res = double.Parse(data["res"]);
+                    data.Clear();
+                }
+                else
+                {
+                    using (StreamWriter writer = new StreamWriter("options.dll"))
+                    {
+                        writer.WriteLine("font = {0}", 16);
+                        writer.WriteLine("time = {0}", true);
+                        writer.WriteLine("rounds = {0}", 1024);
+                        writer.WriteLine("res = {0}", 0);
+                    }
+                }
+            }
+
+            public static void save_opt(int fontout, bool showtime)
+            {
+                //string PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                using (StreamWriter writer = new StreamWriter("options.dll"))
+                {
+                    writer.WriteLine("font = {0}", fontout);
+                    writer.WriteLine("time = {0}", showtime);
+                    writer.WriteLine("rounds = {0}", rounds+1024);
+                    writer.WriteLine("res = {0}", res);
+                }
+
+            }
+            public static void save_res(int rounds_ingame, double restime)
+            {
+                //string PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                using (StreamWriter writer = new StreamWriter("options.dll"))
+                {
+                    if (rounds_ingame == 100)
+                    {
+                        if ((rounds==100 && restime < res) || (rounds<100))
+                        {
+                            writer.WriteLine("font = {0}", font);
+                            writer.WriteLine("time = {0}", time);
+                            writer.WriteLine("rounds = {0}", 1124);
+                            writer.WriteLine("res = {0}", restime);
+                            rounds = 100;
+                            res = restime;
+                        }
+                        else
+                        {
+                            writer.WriteLine("font = {0}", font);
+                            writer.WriteLine("time = {0}", time);
+                            writer.WriteLine("rounds = {0}", 1124);
+                            writer.WriteLine("res = {0}", res);
+                            rounds = 100;
+                        }
+                    }
+                    else
+                    if (rounds_ingame == 99 && rounds<=99 && (restime < res || res==0))
+                    {
+                        writer.WriteLine("font = {0}", font);
+                        writer.WriteLine("time = {0}", time);
+                        writer.WriteLine("rounds = {0}", 1123);
+                        writer.WriteLine("res = {0}", restime);
+                        rounds = 99;
+                        res = restime;
+                    }
+                    else if (rounds_ingame > rounds)
+                    {
+                        writer.WriteLine("font = {0}", font);
+                        writer.WriteLine("time = {0}", time);
+                        writer.WriteLine("rounds = {0}", rounds_ingame + 1024);
+                        writer.WriteLine("res = {0}", 0);
+                        rounds = rounds_ingame;
+                    }
+                    else
+                    {
+                        writer.WriteLine("font = {0}", font);
+                        writer.WriteLine("time = {0}", time);
+                        writer.WriteLine("rounds = {0}", rounds + 1024);
+                        writer.WriteLine("res = {0}", res);
+                    }
+                }
+            }
+        }
+    }
+}
